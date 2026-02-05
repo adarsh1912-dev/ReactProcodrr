9faff535e51294f43d9a95f91e4140410e52c5d0
adarsh-dev <aj19122001@gmail.com>
2026-02-05 18:19:18 +0530

    learnt about parcel

---
+++ 06_parcel/script.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const h1 = <h1>Hello World</h1>
+
+console.log('hello world');
+root.render(h1);
+
+
+
+
+
+
+
+
+
+
+
+
